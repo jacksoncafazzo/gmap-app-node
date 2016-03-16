cc572442bf240c6ab8fc590ad4cddd57474b4f9a
Tim Durward and Will Johnson <student@epicodus.com>
2016-03-16 10:30:45 -0700

    add markers and center gmap

--- js/weather-map.js
+++ js/weather-map.js
@@ -1,7 +1,7 @@
-
 var GMaps = require('gmaps');
-
-$(document).ready(function () {
+//weather api for london:
+//http://api.openweathermap.org/data/2.5/find?lat=51.5073346&lon=-0.1276831&cnt=10&appid=b1b15e88fa797225412429c1c50c122a
+$(document).ready(function() {
     var map = new GMaps({
         div: '#map',
         lat: 51.5073346,
@@ -14,6 +14,27 @@ $(document).ready(function () {
             style: 'SMALL',
             position: 'TOP_LEFT'
         },
-        panControl: false
+        panControl: false,
+    });
+    map.setContextMenu({
+        control: 'map',
+        options: [{
+            title: 'Add marker',
+            name: 'add_marker',
+            action: function(e) {
+                this.addMarker({
+                    lat: e.latLng.lat(),
+                    lng: e.latLng.lng(),
+                    title: 'New marker'
+                });
+            }
+        }, {
+            title: 'Center here',
+            name: 'center_here',
+            action: function(e) {
+                this.setCenter(e.latLng.lat(), e.latLng
+                    .lng());
+            }
+        }]
     });
 });
